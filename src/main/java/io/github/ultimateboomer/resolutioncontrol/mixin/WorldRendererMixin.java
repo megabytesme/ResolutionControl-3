b138e6b5210706d1a9e47cde4fdc54a0b872b1d2
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-01-02 18:54:31 +0000

    Use entityOutlineFramebuffer instead of framebufferset

--- src/main/java/io/github/ultimateboomer/resolutioncontrol/mixin/WorldRendererMixin.java
+++ src/main/java/io/github/ultimateboomer/resolutioncontrol/mixin/WorldRendererMixin.java
@@ -3,32 +3,26 @@ package io.github.ultimateboomer.resolutioncontrol.mixin;
 import io.github.ultimateboomer.resolutioncontrol.ResolutionControlMod;
 import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.DefaultFramebufferSet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.client.util.Handle;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
     @Shadow
-    private DefaultFramebufferSet framebufferSet;
+    public Framebuffer entityOutlineFramebuffer;
 
     @Inject(at = @At("RETURN"), method = "loadEntityOutlinePostProcessor")
     private void onLoadEntityOutlineShader(CallbackInfo ci) {
-        Handle<Framebuffer> entityOutlineHandle = framebufferSet.get(DefaultFramebufferSet.ENTITY_OUTLINE);
-        if (entityOutlineHandle != null && entityOutlineHandle.get() != null) {
-            ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
-        }
+        ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
     }
 
     @Inject(at = @At("RETURN"), method = "onResized")
     private void onOnResized(CallbackInfo ci) {
-        Handle<Framebuffer> entityOutlineHandle = framebufferSet.get(DefaultFramebufferSet.ENTITY_OUTLINE);
-        if (entityOutlineHandle != null && entityOutlineHandle.get() != null) {
-            ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
-        }
+        if (entityOutlineFramebuffer == null)
+            return;
+        ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
     }
 }
