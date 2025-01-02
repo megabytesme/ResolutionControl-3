a954683a81bc60d6edc61da255e6b6e7f0029f39
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-01-02 07:30:23 +0000

    Comment out unused code

--- src/main/java/io/github/ultimateboomer/resolutioncontrol/util/DynamicResolutionHandler.java
+++ src/main/java/io/github/ultimateboomer/resolutioncontrol/util/DynamicResolutionHandler.java
@@ -1,9 +1,7 @@
 package io.github.ultimateboomer.resolutioncontrol.util;
 
-import io.github.ultimateboomer.resolutioncontrol.ResolutionControlMod;
 import it.unimi.dsi.fastutil.floats.FloatArrayList;
 import it.unimi.dsi.fastutil.floats.FloatList;
-import net.minecraft.client.MinecraftClient;
 
 public class DynamicResolutionHandler {
     public static final DynamicResolutionHandler INSTANCE = new DynamicResolutionHandler();
@@ -21,13 +19,12 @@ public class DynamicResolutionHandler {
         timer--;
 
         if (timer <= 0) {
-            //update();
         }
     }
 
     public void reset() {
-        for (float i = Config.getInstance().drMinScale; i <= Config.getInstance().drMaxScale;
-             i += Config.getInstance().drResStep) {
+        for (float i = Config.getInstance().drMinScale; i <= Config.getInstance().drMaxScale; i += Config
+                .getInstance().drResStep) {
             scales.add(i);
 
             if (i == 1.0f) {
@@ -37,37 +34,39 @@ public class DynamicResolutionHandler {
         }
     }
 
-    /*private void update() {
-        MinecraftClient client = MinecraftClient.getInstance();
+    /*
+     * private void update() {
+     * MinecraftClient client = MinecraftClient.getInstance();
+     * 
+     * final int smoothAmount = Config.getInstance().drFpsSmoothAmount;
+     * float sum = 0;
+     * for (int i = client.metricsData.getCurrentIndex() - smoothAmount;
+     * i < client.metricsData.getCurrentIndex(); i++) {
+     * 
+     * sum += client.metricsData.getSamples()[Math.floorMod(i, 240)];
+     * }
+     * float fps = 1_000_000_000.0f / (sum / smoothAmount);
+     * 
+     * if (fps > Config.getInstance().drMaxFps) {
+     * setCurrentScale(Math.min(currentScale + 1, scales.size() - 1));
+     * timer = 15;
+     * } else if (fps < Config.getInstance().drMinFps) {
+     * setCurrentScale(Math.max(currentScale - 1, 0));
+     * timer = 5;
+     * } else {
+     * timer = 3;
+     * }
+     * }
+     */
 
-        final int smoothAmount = Config.getInstance().drFpsSmoothAmount;
-        float sum = 0;
-        for (int i = client.metricsData.getCurrentIndex() - smoothAmount;
-             i < client.metricsData.getCurrentIndex(); i++) {
+    // private void setCurrentScale(int currentScale) {
+    // boolean equal = this.currentScale == currentScale;
+    // this.currentScale = currentScale;
 
-            sum += client.metricsData.getSamples()[Math.floorMod(i, 240)];
-        }
-        float fps = 1_000_000_000.0f / (sum / smoothAmount);
-
-        if (fps > Config.getInstance().drMaxFps) {
-            setCurrentScale(Math.min(currentScale + 1, scales.size() - 1));
-            timer = 15;
-        } else if (fps < Config.getInstance().drMinFps) {
-            setCurrentScale(Math.max(currentScale - 1, 0));
-            timer = 5;
-        } else {
-            timer = 3;
-        }
-    }*/
-
-    private void setCurrentScale(int currentScale) {
-        boolean equal = this.currentScale == currentScale;
-        this.currentScale = currentScale;
-
-        if (!equal) {
-            ResolutionControlMod.getInstance().updateFramebufferSize();
-        }
-    }
+    // if (!equal) {
+    // ResolutionControlMod.getInstance().updateFramebufferSize();
+    // }
+    // }
 
     public double getCurrentScale() {
         return scales.getFloat(currentScale);
