745c080af366eeafebe2c6dc356c3fa272a07188
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-01-02 07:27:07 +0000

    Update world renderer mixin to use DefaultFramebufferSet

--- src/main/java/io/github/ultimateboomer/resolutioncontrol/mixin/WorldRendererMixin.java
+++ src/main/java/io/github/ultimateboomer/resolutioncontrol/mixin/WorldRendererMixin.java
@@ -3,30 +3,32 @@ package io.github.ultimateboomer.resolutioncontrol.mixin;
 import io.github.ultimateboomer.resolutioncontrol.ResolutionControlMod;
 import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.DefaultFramebufferSet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.util.Handle;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
     @Shadow
-    public Framebuffer entityOutlinesFramebuffer;
+    private DefaultFramebufferSet framebufferSet;
 
     @Inject(at = @At("RETURN"), method = "loadEntityOutlinePostProcessor")
     private void onLoadEntityOutlineShader(CallbackInfo ci) {
-        ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
+        Handle<Framebuffer> entityOutlineHandle = framebufferSet.get(DefaultFramebufferSet.ENTITY_OUTLINE);
+        if (entityOutlineHandle != null && entityOutlineHandle.get() != null) {
+            ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
+        }
     }
 
     @Inject(at = @At("RETURN"), method = "onResized")
     private void onOnResized(CallbackInfo ci) {
-        if (entityOutlinesFramebuffer == null) return;
-        ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
+        Handle<Framebuffer> entityOutlineHandle = framebufferSet.get(DefaultFramebufferSet.ENTITY_OUTLINE);
+        if (entityOutlineHandle != null && entityOutlineHandle.get() != null) {
+            ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
+        }
     }
-
-//    @Inject(at = @At("RETURN"), method = "loadTransparencyShader")
-//    private void onLoadTransparencyShader(CallbackInfo ci) {
-//        ResolutionControlMod.getInstance().resizeMinecraftFramebuffers();
-//    }
 }
